870c548ac912ef9878bfaab885d8c7d73657b9e2
Cody <bunchc@gmail.com>
2016-01-10 19:24:56 +0000

    better args & csv

--- read_temps.py
+++ read_temps.py
@@ -3,9 +3,13 @@ import time
 import csv
 from datetime import datetime
 
-# init the 1-wire interface
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
+# Sensor.
+# Need a way to enumerate these, rather than set statically.
+sensor_id = '3b-0000001921e8'
+temp_sensor = '/sys/bus/w1/devices/' + sensor_id + '/w1_slave'
+
+def usage():
+	print "sudo read_temps.py --help --output=[csv, JSON] --cookname=Turkey --datadir=/tmp/sensor_data"
 
 # Get the raw temp from the probe
 def temp_raw():
@@ -28,50 +32,66 @@ def read_temp():
 		temp_f = temp_c * 9.0 / 5.0 + 32.0
 		return temp_c
 
-def usage():
-	print "sudo read_temps.py --output [csv, JSON] --datadir /tmp/sensor_data"
+def csvwriter(data_file, cook_name, t, temp):
+	with open(data_file, 'a', 0) as csvfile:
+		writer = csv.writer(csvfile, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL)
+		writer.writerow([cook_name, t, temp])
 
-def main():
+def main(argv):
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "ho:v", ["help", "output="])
+		opts, args = getopt.getopt(argv, "ho:c:d:c:", ["help", "output=", "cookname=","datadir="])
 	except getopt.GetoptError as err:
 		# print some help thing?
 		print(err)
 		usage()
 		sys.exit(2)
+	
+	#Init some of the things
 	output = None
+	data_dir = None
 	verbose = False
+	cook_name = None
+	
 	for o, a in opts:
-		if o in ("-v", "--verbose"):
-			verbose = True
-		elif o in ("-h", "--help"):
+		if o in ("-h", "--help"):
 			usage()
 			sys.exit(1)
 		elif o in ("-o", "--output"):
 			output = a
+		elif o in ("-c", "--cookname"):
+			cook_name = a
 		elif o in ("-d", "--datadir"):
 			data_dir = a
 		else:
 			assert False, "unhandled option"
 
-if !data_dir = '/home/pi/projects/read_temps/data/'
-sensor_id = '3b-0000001921e8'
-temp_sensor='/sys/bus/w1/devices/' + sensor_id + '/w1_slave'
+	# init the 1-wire interface
+	os.system('modprobe w1-gpio')
+	os.system('modprobe w1-therm')
 
+	# Set or variables
+	data_dir = data_dir or '/home/pi/projects/read_temps/data/'
+	output = output or "csv"
 
-	# Set us up a file to write to.
 	d = datetime.now()
 	date_time = d.strftime('%m%d%Y')
-	data_file = data_dir + date_time + ".csv"
+	cook_name = cook_name or date_time
 
-	with open(data_file, 'wb', 0) as csvfile:
-		csvwriter = csv.writer(csvfile, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL)
-		while True:
-			t = datetime.now().ctime()
-			temp = read_temp()
-			print(t)
-			print(temp)
-			# Write our bits out to a CSV, flush it to a file, then sleep.
-			csvwriter.writerow([t, temp])
-			time.sleep(1)
+	# Main temp read loop
+	while True:
+		temp = read_temp()
+		t = str(time.time())
+		print(t)
+		print(temp)
+		# Write our bits out, then sleep.
+		if output == "csv":
+			data_file = data_dir + cook_name + ".csv"
+			csvwriter(data_file, cook_name, t, temp)
+		elif output == "json":
+			jsonwriter(data_dir, t, temp)
 
+		time.sleep(1)
+
+# Do the things
+if __name__ == "__main__":
+	main(sys.argv[1:])
