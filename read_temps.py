c8f6ab970cafb882466c65dfbd42ea11b3cb1b53
Cody <bunchc@gmail.com>
2016-01-10 23:23:16 +0000

    cleanup

--- read_temps.py
+++ read_temps.py
@@ -1,127 +1,132 @@
-import os, sys, getopt
+
+import os
+import sys
+import getopt
 import time
-import csv, json
+import csv
+import json
 from datetime import datetime
 
 # Sensor.
 # Need a way to enumerate these, rather than set statically.
-sensor_id = '3b-0000001921e8'
-temp_sensor = '/sys/bus/w1/devices/' + sensor_id + '/w1_slave'
+SENSOR_ID = '3b-0000001921e8'
+TEMP_SENSOR = '/sys/bus/w1/devices/' + SENSOR_ID + '/w1_slave'
 
 def usage():
-	print "sudo read_temps.py --help --output=[csv, JSON] --cookname=Turkey --datadir=/tmp/sensor_data"
-	print ""
-	print "	--help		print this help."
-	print " --output 	Set output type as CSV or JSON. CSV is default."
-	print " --cookname 	User friendly name for this cooking session. Defaults to current date."
-	print " --datadir 	Where to store the data collected."
-	print " --display 	Display temp readings on the rPI TFT. Requires CSV."
+    print "sudo read_temps.py --help --output=[csv, JSON] --cookname=Turkey"
+    print "--datadir=/tmp/sensor_data"
+    print ""
+    print " --help      print this help."
+    print " --output    Set output type as CSV or JSON. CSV is default."
+    print " --cookname  User friendly name for this cooking session. Defaults to current date."
+    print " --datadir   Where to store the data collected."
+    print " --display   Display temp readings on the rPI TFT. Requires CSV."
 
 # Get the raw temp from the probe
 def temp_raw():
-	f = open(temp_sensor, 'r')
-	lines = f.readlines()
-	f.close
-	return lines
+    file_reader = open(TEMP_SENSOR, 'r')
+    lines = file_reader.readlines()
+    file_reader.close()
+    return lines
 
-# Make sure the reading is good
+
 def read_temp():
-	lines = temp_raw()
-	while lines[0].strip()[-3] != 'Y':
-		print(lines[0].strip()[-3])
-		time.sleep(0.2)
-		lines = temp_raw()
-	temp_output = lines[1].find('t=')
-	if temp_output != -1:
-		temp_string = lines[1].strip()[temp_output+2:]
-		temp_c = float(temp_string) / 1000.0
-		temp_f = temp_c * 9.0 / 5.0 + 32.0
-		return temp_c
+    lines = temp_raw()
+    while lines[0].strip()[-3] != 'Y':
+        print lines[0].strip()[-3]
+        time.sleep(0.2)
+        lines = temp_raw()
+    temp_output = lines[1].find('t=')
+    if temp_output != -1:
+        temp_string = lines[1].strip()[temp_output + 2:]
+        temp_c = float(temp_string) / 1000.0
+        return temp_c
 
-def csvwriter(data_file, cook_name, t, sensor_id, temp):
-	with open(data_file, 'a', 0) as csvfile:
-		writer = csv.writer(csvfile, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL)
-		writer.writerow([cook_name, sensor_id, t, temp])
 
-def jsonwriter(data_file, cook_name, t, sensor_id, temp):
-	temp_data = {
-		"sensor": sensor_id,
-		"time": t,
-		"temp": temp
-	}
-	
-	if os.path.isfile(data_file) != True:
-		with open(data_file, 'wb', 0) as first_write:
-			header_data = {
-				"cook": cook_name,
-				"startTime": t,
-				"data": [],
-			}
-			json.dump(header_data, first_write)
-	
-	with open(data_file, 'r+', 0) as file:
-		data = json.load(file)
-		data['data'].append(temp_data)
-		file.seek(0)
-		file.write(json.dumps(data))
-		file.truncate
+def csvwriter(data_file, cook_name, epoch_time, sensor_id, temp):
+    with open(data_file, 'a', 0) as csvfile:
+        writer = csv.writer(csvfile, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL)
+        writer.writerow([cook_name, sensor_id, epoch_time, temp])
+
+
+def jsonwriter(data_file, cook_name, epoch_time, sensor_id, temp):
+    temp_data = {
+        "sensor": sensor_id,
+        "time": epoch_time,
+        "temp": temp
+    }
+
+    if os.path.isfile(data_file) != True:
+        with open(data_file, 'wb', 0) as first_write:
+            header_data = {
+                "cook": cook_name,
+                "startTime": epoch_time,
+                "data": [],
+            }
+            json.dump(header_data, first_write)
+
+    with open(data_file, 'r+', 0) as json_file:
+        data = json.load(json_file)
+        data['data'].append(temp_data)
+        json_file.seek(0)
+        json_file.write(json.dumps(data))
+        json_file.truncate()
+
 
 def main(argv):
-	try:
-		opts, args = getopt.getopt(argv, "ho:c:d:c:", ["help", "output=", "cookname=","datadir="])
-	except getopt.GetoptError as err:
-		# print some help thing?
-		print(err)
-		usage()
-		sys.exit(2)
-	
-	#Init some of the things
-	output = None
-	data_dir = None
-	verbose = False
-	cook_name = None
-	
-	for o, a in opts:
-		if o in ("-h", "--help"):
-			usage()
-			sys.exit(1)
-		elif o in ("-o", "--output"):
-			output = a
-		elif o in ("-c", "--cookname"):
-			cook_name = a
-		elif o in ("-d", "--datadir"):
-			data_dir = a
-		else:
-			assert False, "unhandled option"
+    try:
+        opts, args = getopt.getopt(argv, "ho:c:d:c:", ["help", "output=", "cookname=", "datadir="])
+    except getopt.GetoptError as err:
+        # print some help thing?
+        print err
+        usage()
+        sys.exit(2)
 
-	# init the 1-wire interface
-	os.system('modprobe w1-gpio')
-	os.system('modprobe w1-therm')
+    output = None
+    data_dir = None
+    cook_name = None
 
-	# Set or variables
-	data_dir = data_dir or '/home/pi/projects/read_temps/data/'
-	output = output or "csv"
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit(1)
+        elif opt in ("-o", "--output"):
+            output = arg
+        elif opt in ("-c", "--cookname"):
+            cook_name = arg
+        elif opt in ("-d", "--datadir"):
+            data_dir = arg
+        else:
+            assert False, "unhandled option"
 
-	d = datetime.now()
-	date_time = d.strftime('%m%d%Y')
-	cook_name = cook_name or date_time
+    # init the 1-wire interface
+    os.system('modprobe w1-gpio')
+    os.system('modprobe w1-therm')
 
-	# Main temp read loop
-	while True:
-		temp = read_temp()
-		t = str(time.time())
-		print(t)
-		print(temp)
-		# Write our bits out, then sleep.
-		if output == "csv":
-			data_file = data_dir + cook_name + ".csv"
-			csvwriter(data_file, cook_name, t, sensor_id, temp)
-		elif output == "json":
-			data_file = data_dir + cook_name + ".json"
-			jsonwriter(data_file, cook_name, t, sensor_id, temp)
+    # Set or variables
+    data_dir = data_dir or '/home/pi/projects/read_temps/data/'
+    output = output or "csv"
 
-		time.sleep(1)
+    datestring = datetime.now()
+    date_time = datestring.strftime('%m%d%Y')
+    cook_name = cook_name or date_time
+
+    # Main temp read loop
+    while True:
+        temp = read_temp()
+        epoch_time = str(time.time())
+        print epoch_time
+        print temp
+        # Write our bits out, then sleep.
+        if output == "csv":
+            data_file = data_dir + cook_name + ".csv"
+            csvwriter(data_file, cook_name, epoch_time, SENSOR_ID, temp)
+        elif output == "json":
+            data_file = data_dir + cook_name + ".json"
+            jsonwriter(data_file, cook_name, epoch_time, SENSOR_ID, temp)
+
+        time.sleep(1)
 
 # Do the things
 if __name__ == "__main__":
-	main(sys.argv[1:])
+    main(sys.argv[1:])
