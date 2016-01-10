df77bc08422ba4ae6aec636506be2db56a6cefc2
Cody <bunchc@gmail.com>
2016-01-10 04:11:29 +0000

    temps baby

---
+++ read_temps.py
@@ -0,0 +1,51 @@
+import os
+import time
+import csv
+from datetime import datetime
+
+data_dir = "/home/pi/projects/read_temps/data/"
+
+# init the 1-wire interface
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+# This is our sensor.
+# [todo] figure out how to enumerate this automagically
+temp_sensor='/sys/bus/w1/devices/3b-0000001921e8/w1_slave'
+
+# Get the raw temp from the probe
+def temp_raw():
+	f = open(temp_sensor, 'r')
+	lines = f.readlines()
+	f.close
+	return lines
+
+# Make sure the reading is good
+def read_temp():
+	lines = temp_raw()
+	while lines[0].strip()[-3] != 'Y':
+		print(lines[0].strip()[-3])
+		time.sleep(0.2)
+		lines = temp_raw()
+	temp_output = lines[1].find('t=')
+	if temp_output != -1:
+		temp_string = lines[1].strip()[temp_output+2:]
+		temp_c = float(temp_string) / 1000.0
+		temp_f = temp_c * 9.0 / 5.0 + 32.0
+		return temp_c
+
+# Set us up a file to write to.
+d = datetime.now()
+date_time = d.strftime('%m%d%Y')
+data_file = data_dir + date_time + ".csv"
+
+with open(data_file, 'wb', 0) as csvfile:
+	csvwriter = csv.writer(csvfile, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL)
+	while True:
+		t = datetime.now().ctime()
+		temp = read_temp()
+		print(t)
+		print(temp)
+		# Write our bits out to a CSV, flush it to a file, then sleep.
+		csvwriter.writerow([t, temp])
+		time.sleep(1)
