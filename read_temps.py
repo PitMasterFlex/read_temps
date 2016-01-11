1f97a45970dd0945e07e78eca5ac57839f32f120
Cody <bunchc@gmail.com>
2016-01-11 00:30:52 +0000

    moved local_display.py into read_temps.py, removed the dependancy on csv

--- read_temps.py
+++ read_temps.py
@@ -6,11 +6,20 @@ import time
 import csv
 import json
 from datetime import datetime
+import pygame
 
 # Sensor.
 # Need a way to enumerate these, rather than set statically.
 SENSOR_ID = '3b-0000001921e8'
 TEMP_SENSOR = '/sys/bus/w1/devices/' + SENSOR_ID + '/w1_slave'
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
+YELLOW = (255, 255, 0)
 
 def usage():
     print "sudo read_temps.py --help --output=[csv, JSON] --cookname=Turkey"
@@ -20,9 +29,9 @@ def usage():
     print " --output    Set output type as CSV or JSON. CSV is default."
     print " --cookname  User friendly name for this cooking session. Defaults to current date."
     print " --datadir   Where to store the data collected."
-    print " --display   Display temp readings on the rPI TFT. Requires CSV."
+    print " --verbose   Display temp readings on console."
 
-# Get the raw temp from the probe
+
 def temp_raw():
     file_reader = open(TEMP_SENSOR, 'r')
     lines = file_reader.readlines()
@@ -73,9 +82,35 @@ def jsonwriter(data_file, cook_name, epoch_time, sensor_id, temp):
         json_file.truncate()
 
 
+def tft_writer(temp_c):
+    os.putenv('SDL_FBDEV', '/dev/fb1')
+    pygame.init()
+    pygame.mouse.set_visible(False)
+    lcd = pygame.display.set_mode((320, 240))
+    lcd.fill((0, 0, 0))
+    pygame.display.update()
+    background = pygame.Surface(lcd.get_size())
+    background = background.convert()
+    background.fill(WHITE)
+    box = pygame.draw.rect(background, YELLOW, (40, 0, 40, 240))
+    box = pygame.draw.rect(background, CYAN, (80, 0, 40, 240))
+    box = pygame.draw.rect(background, GREEN, (120, 0, 40, 240))
+    box = pygame.draw.rect(background, MAGENTA, (160, 0, 40, 240))
+    box = pygame.draw.rect(background, RED, (200, 0, 40, 240))
+    box = pygame.draw.rect(background, BLUE, (240, 0, 40, 240))
+    box = pygame.draw.rect(background, BLACK, (280, 0, 40, 240))
+    font_big = pygame.font.Font(None, 60)
+    temp = temp_c * 9.0 / 5.0 + 32.0
+    lcd.fill((0, 0, 0))
+    text_surface = font_big.render('Temp: ' + str(temp) + "F", True, WHITE)
+    rect = text_surface.get_rect(center=(160, 120))
+    lcd.blit(text_surface, rect)
+    pygame.display.update()
+
+
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv, "ho:c:d:c:", ["help", "output=", "cookname=", "datadir="])
+        opts, args = getopt.getopt(argv, "ho:c:d:vt", ["help", "output=", "cookname=", "datadir=", "verbose", "tft"])
     except getopt.GetoptError as err:
         # print some help thing?
         print err
@@ -85,6 +120,8 @@ def main(argv):
     output = None
     data_dir = None
     cook_name = None
+    verbose = False
+    tft = False
 
     for opt, arg in opts:
         if opt in ("-h", "--help"):
@@ -96,6 +133,10 @@ def main(argv):
             cook_name = arg
         elif opt in ("-d", "--datadir"):
             data_dir = arg
+        elif opt in ("-v", "--verbose"):
+            verbose = True
+        elif opt in ("-t", "--tft"):
+            tft = True
         else:
             assert False, "unhandled option"
 
@@ -111,22 +152,21 @@ def main(argv):
     date_time = datestring.strftime('%m%d%Y')
     cook_name = cook_name or date_time
 
-    # Main temp read loop
     while True:
         temp = read_temp()
         epoch_time = str(time.time())
-        print epoch_time
-        print temp
-        # Write our bits out, then sleep.
+        if tft:
+            tft_writer(temp)
+        if verbose:
+            print str(epoch_time) + ", " + str(temp)
         if output == "csv":
             data_file = data_dir + cook_name + ".csv"
             csvwriter(data_file, cook_name, epoch_time, SENSOR_ID, temp)
         elif output == "json":
             data_file = data_dir + cook_name + ".json"
             jsonwriter(data_file, cook_name, epoch_time, SENSOR_ID, temp)
-
         time.sleep(1)
 
-# Do the things
+
 if __name__ == "__main__":
     main(sys.argv[1:])
