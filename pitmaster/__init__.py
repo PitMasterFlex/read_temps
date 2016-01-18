e86e1accadcc440b426b439040e3853872a686dd
Michael Rice <michael.rice@rackspace.com>
2016-01-18 02:19:14 -0600

    added a basic setup.py file and example executable
    added some other misc files for packages..

---
+++ pitmaster/__init__.py
@@ -0,0 +1,19 @@
+#   Copyright 2016 Michael Rice <michael@michaelrice.org>
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+__author__ = ["Cody Bunch", "Michael Rice"]
+__copyright__ = "2015 All Rights Reserved"
+__version__ = "0.1"
+__url__ = 'https://github.com/bunchc/read_temps'
+__email__ = ["", "michael@michaelrice.org"]
